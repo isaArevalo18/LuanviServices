267bac6aca51f0f24223bd88c556a1ecb51dcab6
isaArevalo18 <isaArevalo18@users.noreply.github.com>
2020-10-05 12:50:50 -0500

    Login WEB

---
+++ Rest/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Principal</title>
+	<link rel="stylesheet"  href="css/estilos.css">
+</head>
+<body background="botones.jpeg">
+	<div class="rj-layout">
+		<button class="rj-btn">Empleados</button>
+		<button class="rj-btn">Clientes</button>
+		<button class="rj-btn">Productos</button>
+
+	</div>
+</body>
+</html>
---
+++ Rest/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width-device-width,initial-scale-1.0"> 
+	<title>Login</title>
+	<link rel="stylesheet"  href="css/login.css">
+	<link rel="stylesheet"  href="css/cabecera.css">
+</head>
+<body>
+<form action="validar.php" method="post">
+<h1>Sistema login</h1>
+<p>Usuario <input type="text" placeholder="Ingrese su nombre" name="usuario"></p>
+<p>Contraseña <input type="text" placeholder="Ingrese su contraseña" name="clave"></p>
+<input type="submit" value="Ingresar">
+</form>
+</body>
+</html>
